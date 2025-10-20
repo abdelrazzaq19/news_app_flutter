5221a8a617c7f2bade1eec664bc372b5ed5a674f
M.ziad abdel razzaq <abdelrazzaq19@gmail.com>
2025-10-21 02:32:14 +0700

    first commit

---
+++ lib/pages/article_view.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+
+class ArticleView extends StatefulWidget {
+  final String blogUrl;
+  const ArticleView({super.key, required this.blogUrl});
+
+  @override
+  State<ArticleView> createState() => _ArticleViewState();
+}
+
+class _ArticleViewState extends State<ArticleView> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Inisialisasi controller WebView
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0xFFFFFFFF))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (progress) {
+            // progress loading (0 - 100)
+          },
+          onPageStarted: (url) {
+            debugPrint('Started: $url');
+          },
+          onPageFinished: (url) {
+            debugPrint('Finished: $url');
+          },
+          onWebResourceError: (error) {
+            debugPrint('Error: ${error.description}');
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(widget.blogUrl));
+
+    // Android-specific feature (opsional)
+    if (WebViewPlatform.instance is AndroidWebViewPlatform) {
+      AndroidWebViewController.enableDebugging(true);
+      (WebViewPlatform.instance as AndroidWebViewPlatform)
+          .useHybridComposition = true;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Article'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: WebViewWidget(controller: _controller),
+    );
+  }
+}
+
+extension on AndroidWebViewPlatform {
+  set useHybridComposition(bool useHybridComposition) {}
+}
---
+++ lib/services/news.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:news_app/models/article_model.dart';
+
+class News {
+  List<ArticleModel> news = [];
+
+  Future<void> getNews() async {
+    String url =
+        "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=6f4f1c8957004142ad853cbc61e833f4";
+    var response = await http.get(Uri.parse(url));
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == 'ok') {
+      jsonData["articles"].forEach((element) {
+        if (element["urlToImage"] != null && element['description'] != null) {
+          ArticleModel articleModel = ArticleModel(
+            title: element['title'],
+            description: element['description'],
+            url: element['url'],
+            urlToImage: element['urlToImage'],
+            content: element["content"],
+            author: element["author"],
+          );
+          news.add(articleModel);
+        }
+      });
+    }
+  }
+}
